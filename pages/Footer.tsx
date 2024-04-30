e97284dba99bfbab95063af550083343ddb1ace9
Wik21 <putuwikadinata21@gmail.com>
2024-04-30 11:29:54 +0800

    wik

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className="bg-[#0a5d4f] text-white font-nunito w-s w-full">
       <div className="px-5 pt-4">
-        <div className="md:flex md:justify-between">
-        <div className="">
+        <div className="o">
+          <div className="md:flex md:justify-between">
           <div>
           <h1 className="text-2xl font-bold mt-3 md:mt-0 md:text-left">
               Villa
@@ -21,30 +21,34 @@ const Footer = () => {
             <p className="text-sm max-w-xs text-justify md:text-left">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, architecto atque dolor libero eaque delectus!
             </p>
-          <div className="">
-          <div className="mt-3 flex">
-              {/* <Image src={house} alt="House"></Image>
-              <p className="ml-2">Jl Pucuk 1 No 99x Banjar Tangtu</p>
             </div>
-            <div className="mt-1 flex">            
-              <Image src={phone} alt="Phone"></Image>
-              <p className="ml-2">0812-3902-9304</p>              
+
+            <div>
+          <h1 className="text-2xl font-bold mt-3 md:mt-0 md:text-left">
+              Villa
+            </h1>
+            <p className="text-sm max-w-xs text-justify md:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, architecto atque dolor libero eaque delectus!
+            </p>
+            <br/>
+            <h1 className="text-2xl font-bold mt-3 md:mt-0 md:text-left">
+              Social Media
+            </h1>
+            <p className="text-sm max-w-xs text-justify md:text-left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, architecto atque dolor libero eaque delectus!
+            </p>
             </div>
-            <div className="mt-1 flex">            
-              <Image src={envelope} alt="Email"></Image>
-              <p className="ml-2">bastiankristiawan9@gmail.com</p>               */}
+
+            <div>
+          <h1 className="text-2xl font-bold mt-3 md:mt-0 md:text-left">
+              Team Work
+            </h1>
+            <p className="text-sm max-w-xs text-justify md:text-left">
+            
+            </p>
+          </div>
             </div>
           </div>
-          </div>
-          </div>
-          <div className="md:flex md:items-center mt-4">
-          <div className="md:text-right ">
-            <h1 className="text-2xl font-bold">Social Media</h1>
-            <div className="flex  space-x-2">
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
       <hr className="border-white opacity-60 mt-1"></hr>
       <p className="text-sm opacity-60 flex flex-col items-center justify-center mt-2 pb-2 ">
